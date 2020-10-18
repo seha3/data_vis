2d82d5d046e9559a7e86169ccf8d35fa83ed60fd
seha13 <out_sherreraa@independencia.com.mx>
2020-10-17 20:53:32 -0500

    se agregó dropdown de estados

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+// import states from '../catalogues/states.json'
+import SimpleSelect from './components/SimpleSelect'
 
 function App() {
   return (
     <div className="App">
-      <h1>hola</h1>
+      <h1>Data Vis</h1>
+      <SimpleSelect />
+     
     </div>
   );
 }
---
+++ src/components/SimpleSelect.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+
+function SimpleSelect() {
+    return (
+        <div className="dropdown">
+            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Dropdown States
+            </button>
+          <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a className="dropdown-item" href="/">Aguascalientes</a>
+            <a className="dropdown-item" href="/">Baja California</a>
+            <a className="dropdown-item" href="/">Baja California Sur</a>
+            <a className="dropdown-item" href="/">Campeche</a>
+            <a className="dropdown-item" href="/">Chiapas</a>
+            <a className="dropdown-item" href="/">Chihuahua</a>
+            <a className="dropdown-item" href="/">Coahuila</a>
+            <a className="dropdown-item" href="/">Colima</a>
+            <a className="dropdown-item" href="/">Ciudad de México</a>
+            <a className="dropdown-item" href="/">Durango</a>
+            <a className="dropdown-item" href="/">Estado de México</a>
+            <a className="dropdown-item" href="/">Guanajuato</a>
+            <a className="dropdown-item" href="/">Guerrero</a>
+            <a className="dropdown-item" href="/">Hidalgo</a>
+            <a className="dropdown-item" href="/">Jalisco</a>
+            <a className="dropdown-item" href="/">Michoacán</a>
+            <a className="dropdown-item" href="/">Morelos</a>
+            <a className="dropdown-item" href="/">Nayarit</a>
+            <a className="dropdown-item" href="/">Nuevo León</a>
+            <a className="dropdown-item" href="/">Oaxaca</a>
+            <a className="dropdown-item" href="/">Puebla</a>
+            <a className="dropdown-item" href="/">Querétaro</a>
+            <a className="dropdown-item" href="/">Quintana Roo</a>
+            <a className="dropdown-item" href="/">San Luis Potosí</a>
+            <a className="dropdown-item" href="/">Sinaloa</a>
+            <a className="dropdown-item" href="/">Sonora</a>
+            <a className="dropdown-item" href="/">Tabasco</a>
+            <a className="dropdown-item" href="/">Tamaulipas</a>
+            <a className="dropdown-item" href="/">Tlaxcala</a>
+            <a className="dropdown-item" href="/">Veracruz</a>
+            <a className="dropdown-item" href="/">Yucatán</a>
+            <a className="dropdown-item" href="/">Zacatecas</a>
+          </div>
+        </div>
+    )
+}
+
+export default SimpleSelect
+
