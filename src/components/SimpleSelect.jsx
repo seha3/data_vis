e7d1f08d18e797c9780294454676f4d07de9693c
seha13 <out_sherreraa@independencia.com.mx>
2020-10-20 01:09:38 -0500

    datos de estados para states

--- src/components/SimpleSelect.jsx
+++ src/components/SimpleSelect.jsx
@@ -13,38 +13,38 @@ export default class SimpleSelect extends Component {
                     Estado
                 </button>
                 <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a className="dropdown-item" href="/">Aguascalientes</a>
-                    <a className="dropdown-item" href="/">Baja California</a>
-                    <a className="dropdown-item" href="/">Baja California Sur</a>
-                    <a className="dropdown-item" href="/">Campeche</a>
-                    <a className="dropdown-item" href="/">Chiapas</a>
-                    <a className="dropdown-item" href="/">Chihuahua</a>
-                    <a className="dropdown-item" href="/">Coahuila</a>
-                    <a className="dropdown-item" href="/">Colima</a>
-                    <a className="dropdown-item" href="/">Ciudad de México</a>
-                    <a className="dropdown-item" href="/">Durango</a>
-                    <a className="dropdown-item" href="/">Estado de México</a>
-                    <a className="dropdown-item" href="/">Guanajuato</a>
-                    <a className="dropdown-item" href="/">Guerrero</a>
-                    <a className="dropdown-item" href="/">Hidalgo</a>
-                    <a className="dropdown-item" href="/">Jalisco</a>
-                    <a className="dropdown-item" href="/">Michoacán</a>
-                    <a className="dropdown-item" href="/">Morelos</a>
-                    <a className="dropdown-item" href="/">Nayarit</a>
-                    <a className="dropdown-item" href="/">Nuevo León</a>
-                    <a className="dropdown-item" href="/">Oaxaca</a>
-                    <a className="dropdown-item" href="/">Puebla</a>
-                    <a className="dropdown-item" href="/">Querétaro</a>
-                    <a className="dropdown-item" href="/">Quintana Roo</a>
-                    <a className="dropdown-item" href="/">San Luis Potosí</a>
-                    <a className="dropdown-item" href="/">Sinaloa</a>
-                    <a className="dropdown-item" href="/">Sonora</a>
-                    <a className="dropdown-item" href="/">Tabasco</a>
-                    <a className="dropdown-item" href="/">Tamaulipas</a>
-                    <a className="dropdown-item" href="/">Tlaxcala</a>
-                    <a className="dropdown-item" href="/">Veracruz</a>
-                    <a className="dropdown-item" href="/">Yucatán</a>
-                    <a className="dropdown-item" href="/">Zacatecas</a>
+                  <a className="dropdown-item" href="/" id="1" estados="AGS">Aguascalientes</a>
+                  <a className="dropdown-item" href="/" id="2" estados="BCN">Baja California</a>
+                  <a className="dropdown-item" href="/" id="3" estados="BCS">Baja California Sur</a>
+                  <a className="dropdown-item" href="/" id="4" estados="CAM">Campeche</a>
+                  <a className="dropdown-item" href="/" id="5" estados="CHS">Chiapas</a>
+                  <a className="dropdown-item" href="/" id="6" estados="CHI">Chihuahua</a>
+                  <a className="dropdown-item" href="/" id="7" estados="COA">Coahuila</a>
+                  <a className="dropdown-item" href="/" id="8" estados="COL">Colima</a>
+                  <a className="dropdown-item" href="/" id="9" estados="CDMX">Ciudad de México</a>
+                  <a className="dropdown-item" href="/" id="10" estados="DGO">Durango</a>
+                  <a className="dropdown-item" href="/" id="11" estados="EM">Estado de México</a>
+                  <a className="dropdown-item" href="/" id="12" estados="GTO">Guanajuato</a>
+                  <a className="dropdown-item" href="/" id="13" estados="GRO">Guerrero</a>
+                  <a className="dropdown-item" href="/" id="14" estados="HGO">Hidalgo</a>
+                  <a className="dropdown-item" href="/" id="15" estados="JAL">Jalisco</a>
+                  <a className="dropdown-item" href="/" id="16" estados="MICH">Michoacán</a>
+                  <a className="dropdown-item" href="/" id="17" estados="MOR">Morelos</a>
+                  <a className="dropdown-item" href="/" id="18" estados="NAY">Nayarit</a>
+                  <a className="dropdown-item" href="/" id="19" estados="NL">Nuevo León</a>
+                  <a className="dropdown-item" href="/" id="20" estados="OAX">Oaxaca</a>
+                  <a className="dropdown-item" href="/" id="21" estados="PUE">Puebla</a>
+                  <a className="dropdown-item" href="/" id="22" estados="QRO">Querétaro</a>
+                  <a className="dropdown-item" href="/" id="23" estados="QR">Quintana Roo</a>
+                  <a className="dropdown-item" href="/" id="24" estados="SLP">San Luis Potosí</a>
+                  <a className="dropdown-item" href="/" id="25" estados="SIN">Sinaloa</a>
+                  <a className="dropdown-item" href="/" id="26" estados="SON">Sonora</a>
+                  <a className="dropdown-item" href="/" id="27" estados="TAB">Tabasco</a>
+                  <a className="dropdown-item" href="/" id="28" estados="TAM">Tamaulipas</a>
+                  <a className="dropdown-item" href="/" id="29" estados="TLAX">Tlaxcala</a>
+                  <a className="dropdown-item" href="/" id="30" estados="VER">Veracruz</a>
+                  <a className="dropdown-item" href="/" id="31" estados="YUC">Yucatán</a>
+                  <a className="dropdown-item" href="/" id="32" estados="ZAC">Zacatecas</a>
                 </div>
             
                 <div className="dropdown d-inline p-2">
